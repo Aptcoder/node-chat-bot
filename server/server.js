29f1673263bda8879e5e11a91dbd1f5c605c376a
Aptcoder <omilosamuel@gmail.com>
2020-05-18 15:00:21 +0100

    fix : add port variable

--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ let app = express();
 let publicPath = path.join(__dirname,'../public')
 app.use(express.static(publicPath))
 
+let port = process.env.PORT || 3000;
+
 
 
 
 
 console.log(publicPath);
 
-app.listen(3000,() => {
+app.listen(port,() => {
     console.log('node is listening in port 3000')
 })
